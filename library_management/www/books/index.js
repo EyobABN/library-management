f6a7863dfcde830356c5563263b86ee9aba9c996
EyobABN <eyob.alamerew.belay@gmail.com>
2024-01-31 15:30:18 +0300

    Implement book add and delete features

--- library_management/www/books/index.js
+++ library_management/www/books/index.js
@@ -1,5 +1,5 @@
-// Function to open the edit modal with book details
-function openModal(docName='') {
+// Function to initialize the form modal by optionally populating the form fields with book details
+function initModal(docName='') {
   if (docName) {
     // Load document to be updated
     frappe.call({
@@ -11,11 +11,12 @@ function openModal(docName='') {
       callback: function(response) {
         if (response.message) {
           const book = response.message;
-          const modal = document.getElementById("modal");
-          modal.style.display = "block";
         
-          document.getElementById("modalHeader").textContent = book.title;
-        
+          document.getElementById("formModalLabel").innerText = "Update Book";
+          const submitButton = document.getElementById("submitButton");
+          submitButton.innerText = "Update Book";
+          submitButton.onclick = updateDoc;
+
           // Populate the form fields with book details
           document.getElementById("docName").value = book.name;
           document.getElementById("title").value = book.title;
@@ -34,84 +35,158 @@ function openModal(docName='') {
       }
     });
   } else {
-    const modal = document.getElementById("modal");
-    modal.style.display = "block";
+    // Display an empty form for adding a new document
+    document.getElementById("formModalLabel").innerText = "Add a new Book";
+    const submitButton = document.getElementById("submitButton");
+    submitButton.innerText = "Add Book";
+    submitButton.onclick = addDoc;
 
-    document.getElementById("modalHeader").innerText = "Add a new book";
-    document.getElementById("submitButton").innerText = "Add Book"
+    // Reset the form fields in case they have been populated from a previous update command
+    document.getElementById("docName").value = '';
+    document.getElementById("title").value = '';
+    document.getElementById("author").value = '';
+    document.getElementById("isbn").value = '';
+    document.getElementById("genre").value = '';
+    document.getElementById("publication_year").value = '';
+    document.getElementById("status").value = '';
   }
 }
 
-// Function to close the edit modal
-function closeEditModal() {
-  const modal = document.getElementById("modal");
-  modal.style.display = "none";
-}
+// Function to add a doc
+function addDoc() {
+  const docData = {};
+  
+  // Prepare data to be sent as payload in post request
+  docData['title'] = document.getElementById('title').value;
+  docData['author'] = document.getElementById('author').value;
+  docData['publication_year'] = document.getElementById('publication_year').value;
+  docData['genre'] = document.getElementById('genre').value;
+  const isbn = document.getElementById('isbn').value;
+  if (isbn) docData['isbn'] = isbn;
+  docData['status'] = document.getElementById('status').value;
 
-// Function to update the book
-function updateDoc() {
-  const docName = document.getElementById("docName").value;
-  const updateData = {}
-  const updateDocInBackend = () => {
-    frappe.call({
-      method: 'library_management.api.book_api.update_book',
-      args: {
-        book_name: docName,
-        update_data: updateData
-      },
-      callback: (r) => {
-        window.location.reload(true);
-        console.log(r.message);
-      },
-      error: (r) => {
-        window.location.reload(true);
-        console.error(r);
-      }
-    })
-  }
-  // Prepare update data to be sent as payload in post request
-  updateData['title'] = document.getElementById('title').value;
-  updateData['author'] = document.getElementById('author').value;
-  updateData['publication_year'] = document.getElementById('publication_year').value;
-  updateData['genre'] = document.getElementById('genre').value;
-  updateData['isbn'] = document.getElementById('isbn').value;
-  updateData['status'] = document.getElementById('status').value;
-
-  // Upload image file
+  // if a file has been selected, upload it and then update the doc
   const file = document.getElementById('image').files[0];
   if (file) {
+    docData['image'] = '/files/' + file.name;
+    uploadFile(file)
+      .then(() => {
+        addDocInBackend(docData);
+      })
+      .catch(e => console.error(e));
+  } else {
+    addDocInBackend(docData);
+  }
+}
+
+// Function to delete a doc
+function deleteDoc() {
+  const docName = document.getElementById("docName").value;
+  frappe.call({
+    method: 'library_management.api.book_api.delete_book',
+    args: {
+      book_name: docName,
+    },
+    callback: () => {
+      console.log('Document deleted successfully');
+      window.location.reload(true);
+    },
+    error: (r) => {
+      console.error(r);
+    }
+  })
+}
+
+// Function to update the document
+function updateDoc() {
+  const docName = document.getElementById("docName").value;
+  const docData = {};
+  
+  // Prepare update data to be sent as payload in PUT request
+  docData['title'] = document.getElementById('title').value;
+  docData['author'] = document.getElementById('author').value;
+  docData['publication_year'] = document.getElementById('publication_year').value;
+  docData['genre'] = document.getElementById('genre').value;
+  const isbn = document.getElementById('isbn').value;
+  if (isbn) docData['isbn'] = isbn;
+  docData['status'] = document.getElementById('status').value;
+
+  // if a file has been selected, upload it and then update the doc
+  const file = document.getElementById('image').files[0];
+  if (file) {
+    docData['image'] = '/files/' + file.name;
+    uploadFile(file)
+      .then(() => {
+        updateDocInBackend(docName, docData);
+      })
+      .catch(e => console.error(e));
+  } else {
+    updateDocInBackend(docName, docData);
+  }
+}
+
+// Function to upload a file to the Frappe backend
+function uploadFile(file) {
+  return new Promise((resolve, reject) => {
     const xhr = new XMLHttpRequest();
     xhr.open('POST', '/api/method/upload_file', true);
     xhr.setRequestHeader('Accept', 'application/json');
     xhr.setRequestHeader('X-Frappe-CSRF-Token', frappe.csrf_token);
-  
+
     let form_data = new FormData();
-  
     form_data.append('file', file, file.name);
-  
-    form_data.append('doctype', 'Book');
-    form_data.append('docname', docName);
-  
+
     // Add event listeners
     xhr.onload = function() {
       if (xhr.status == 200) {
-        updateData['image'] = '/files/' + file.name
         console.log("File uploaded successfully");
-
-        updateDocInBackend();
+        resolve(xhr.response);
       } else {
         console.error("File upload failed with status " + xhr.status);
-        window.location.reload(true);
+        reject(new Error("File upload failed with status " + xhr.status));
       }
     };
-  
+
     xhr.onerror = function() {
       console.error("Request failed");
-      window.location.reload(true);
+      reject(new Error("Request failed"));
     };
-  
+
     xhr.send(form_data);
-  } else {
-    updateDocInBackend();
-  }
+  });
+}
+
+// Function to add a document to the Frappe backend
+function addDocInBackend(docData) {
+  frappe.call({
+    method: 'library_management.api.book_api.create_book',
+    args: {
+      book_data: docData
+    },
+    callback: (r) => {
+      window.location.reload(true);
+      console.log(r.message);
+    },
+    error: (r) => {
+      console.error(r);
+    }
+  })
+}
+
+// Function to update a document in the Frappe backend
+function updateDocInBackend(docName, docData) {
+  frappe.call({
+    method: 'library_management.api.book_api.update_book',
+    args: {
+      book_name: docName,
+      update_data: docData
+    },
+    callback: (r) => {
+      window.location.reload(true);
+      console.log(r.message);
+    },
+    error: (r) => {
+      console.error(r);
+    }
+  })
 }
